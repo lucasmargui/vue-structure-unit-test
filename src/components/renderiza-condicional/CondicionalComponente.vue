d4d489dc32ec4fac0d40063ebd53a3ff659fbdb8
Lucas Martins Guimarães <lucasmargui@outlook.com>
2024-03-21 09:37:27 -0300

    update: update condicional teste unitário

---
+++ src/components/renderiza-condicional/CondicionalComponente.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p v-if="mostrarMensagem">{{ mensagem }}</p>
+    <button @click="toggleMensagem">Toggle Mensagem</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mostrarMensagem: false,
+      mensagem: 'Olá, mundo!'
+    };
+  },
+  methods: {
+    toggleMensagem() {
+        this.mostrarMensagem = !this.mostrarMensagem;
+      console.log('mostrarMensagem:', this.mostrarMensagem);
+    }
+  }
+};
+</script>
